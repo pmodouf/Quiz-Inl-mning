f748f7ad80f8d38ec61a8d14534bdd7aa510f43d
knottem <59744487+knottem@users.noreply.github.com>
2022-11-20 23:15:41 +0100

    client_smallfix

--- src/client/ClientProtocol.java
+++ src/client/ClientProtocol.java
@@ -1,19 +1,15 @@
 package client;
 
-import gamepackage.GamePackage;
-
 public class ClientProtocol {
     Client client;
 
     public ClientProtocol(Client client){
         this.client = client;
-
     }
 
-    public void  update(){
+    public void update(){
         client.gp.setMessage("Hej det här är ifrån klienten. ");
         System.out.println(client.gp);
-
     }
 
 }
