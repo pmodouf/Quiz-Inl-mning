ff3a2dee5a33d296da602eabb4bef8cb0478d2ac
knottem <59744487+knottem@users.noreply.github.com>
2022-11-22 15:54:28 +0100

    lite serverProtocol bugfix, ServerListener mindre kod, och clienthandler hantera socketException.

--- src/server/ServerListener.java
+++ src/server/ServerListener.java
@@ -2,15 +2,11 @@ package server;
 
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ServerListener {
 
     final private int port = 12345;
-
-    int count = 0;
     ServerProtocol spHolder;
-    ClientHandler chHolder;
 
     ServerListener() {
         try (ServerSocket server = new ServerSocket(port)) {
@@ -23,18 +19,11 @@ public class ServerListener {
     }
 
     private void connectClient(ServerSocket server) throws IOException {
-        Socket client = server.accept();
-        count++;
-        if(count ==1){
-            spHolder = new ServerProtocol();
-            chHolder = new ClientHandler(client, spHolder, count);
-        }else{
-            ClientHandler client1 = chHolder;
-            ClientHandler client2 = new ClientHandler(client, spHolder, count);
-            client1.start();
-            client2.start();
-            count = 0;
-        }
+        spHolder = new ServerProtocol();
+        ClientHandler client1 = new ClientHandler(server.accept(), spHolder, 1);
+        ClientHandler client2 = new ClientHandler(server.accept(), spHolder, 2);
+        client1.start();
+        client2.start();
     }
 
     public static void main(String[] args) {
