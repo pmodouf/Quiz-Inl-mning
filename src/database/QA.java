1c472389b7598d8bb44f3909b8fa7e706e270ad6
knottem <59744487+knottem@users.noreply.github.com>
2022-11-24 02:32:12 +0100

    litet program för att testa qa

--- src/database/QA.java
+++ src/database/QA.java
@@ -10,7 +10,11 @@ import java.util.Scanner;
 
 public class QA {
 
-    private final ArrayList<ArrayList<String[]>> qaList;
+    private final Random random = new Random();
+
+    private ArrayList<String[]> qaList;
+
+    private String category = "";
 
     private final String[] categories = {"Historia","Sport","Musik", "Samhälle", "Vetenskap", "Geografi"};
 
@@ -19,19 +23,21 @@ public class QA {
     }
 
     public void loadQA(int i){
-        qaList.removeAll(Collections.singleton(null));
+        //qaList.removeAll(Collections.singleton(null));
         switch (i){
-            case 1 -> qaList.add(loadFiles(categories[0]));
-            case 2 -> qaList.add(loadFiles(categories[1]));
-            case 3 -> qaList.add(loadFiles(categories[2]));
-            case 4 -> qaList.add(loadFiles(categories[3]));
-            case 5 -> qaList.add(loadFiles(categories[4]));
-            case 6 -> qaList.add(loadFiles(categories[5]));
+            case 1 -> qaList = (loadFiles(categories[0]));
+            case 2 -> qaList = (loadFiles(categories[1]));
+            case 3 -> qaList = (loadFiles(categories[2]));
+            case 4 -> qaList = (loadFiles(categories[3]));
+            case 5 -> qaList = (loadFiles(categories[4]));
+            case 6 -> qaList = (loadFiles(categories[5]));
+            default -> qaList = (loadFiles(categories[random.nextInt(categories.length)+1]));
         }
     }
 
     private ArrayList<String[]> loadFiles (String filename){
         String line;
+        category = filename;
         try(BufferedReader reader = new BufferedReader(new FileReader("src/resources/categories/" + filename + ".txt"))){
             ArrayList<String[]> temp = new ArrayList<>();
             while((line = reader.readLine()) != null) {
@@ -45,35 +51,42 @@ public class QA {
         }
     }
 
-    public String[] getCategories() {
-        return categories;
+    public String getCategory() {
+        return category;
     }
 
     public ArrayList<String[]> getList() {
-        return qaList.get(0);
+        return qaList;
     }
 
     public static void main(String[] args) {
         QA qa = new QA();
-        Random random = new Random();
-        int val = random.nextInt(6)+1;
 
-        qa.loadQA(val);
-        int score = 0;
-        System.out.println(qa.getCategories()[val-1]);
-        for (int i = 0; i <qa.getList().size() ; i++) {
-            System.out.println(qa.getList().get(i)[0]);
-            System.out.println(qa.getList().get(i)[1] + " eller " + qa.getList().get(i)[2] + " eller " + qa.getList().get(i)[3] + " eller " + qa.getList().get(i)[4]);
-            Scanner scan = new Scanner(System.in);
-            String answer = scan.nextLine();
-            if (answer.equalsIgnoreCase(qa.getList().get(i)[5])) {
-                System.out.println("CORRECT");
-                score++;
-            } else {
-                System.out.println("WRONG");
+        //Exempel till koden
+        //Första. gp.setQA(qa.loadQA(gp.getCategoryID));
+        //Andra. gp.setQA(qa);
+        //
+        //
+        //ifall 0 eller högre än dem kategorierna som finns så blir det random, annars så väljer man
+
+        for (int j = 0; j < 2; j++) {
+            qa.loadQA(0);
+            int score = 0;
+            System.out.println(qa.getCategory());
+            for (int i = 0; i < qa.getList().size(); i++) {
+                System.out.println(qa.getList().get(i)[0]);
+                System.out.println(qa.getList().get(i)[1] + " eller " + qa.getList().get(i)[2] + " eller " + qa.getList().get(i)[3] + " eller " + qa.getList().get(i)[4]);
+                Scanner scan = new Scanner(System.in);
+                String answer = scan.nextLine();
+                if (answer.equalsIgnoreCase(qa.getList().get(i)[5])) {
+                    System.out.println("CORRECT");
+                    score++;
+                } else {
+                    System.out.println("WRONG");
+                }
             }
+            System.out.println("TotalScore: " + score + " out of " + qa.getList().size());
         }
-        System.out.println("TotalScore: " + score + " out of " + qa.getList().size());
     }
 }
 
